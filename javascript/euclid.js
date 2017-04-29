a1e3ef9080f7ef6bb2a3d28a7f42547bc33d303d
Andrey-Shatunov <andreshatuno@gmail.com>
2017-04-29 11:12:00 +0500

    Create euclid.js

---
+++ javascript/euclid.js
@@ -0,0 +1,14 @@
+function euclid(a, b) {
+  if(b==0){
+  	return [a,1,0];
+  }
+  else{
+  	var mass=euclid(b,a%b);
+    var i=mass[2];
+    var j=mass[1]-(a/b*mass[2]);
+    return [mass[0],i,j];
+  }
+}
+
+mass=euclid(12, 16);
+alert(mass[0]);
