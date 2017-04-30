e58fb3018782b82b04833fd5d2b440aaa7908d96
Andrey-Shatunov <andreshatuno@gmail.com>
2017-04-30 13:03:36 +0500

    Update euclid.js

--- javascript/euclid.js
+++ javascript/euclid.js
@@ -1,14 +1,15 @@
 function euclid(a, b) {
-  if(b==0){
-  	return [a,1,0];
+  
+  if(b == 0){
+  	return [a, 1, 0];
   }
   else{
-  	var mass=euclid(b,a%b);
-    var i=mass[2];
-    var j=mass[1]-(a/b*mass[2]);
-    return [mass[0],i,j];
+  	var mass=euclid(b, a%b);
+    var i = mass[2];
+    var j = mass[1] - (a / b * mass[2]);
+    return [mass[0], i , j];
   }
 }
 
 mass=euclid(12, 16);
-alert(mass[0]);
+alert( mass[0] );
