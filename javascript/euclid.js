faf45639803d34da284317f20412a825dd1329dd
Andrey-Shatunov <andreshatuno@gmail.com>
2017-04-30 13:08:31 +0500

    Update euclid.js

--- javascript/euclid.js
+++ javascript/euclid.js
@@ -3,7 +3,7 @@ function euclid(a, b) {
   if(b == 0)
   	return [a, 1, 0];
   else{
-  	var mass=euclid(b, a%b);
+  	var mass = euclid(b, a%b);
     var i = mass[2];
     var j = mass[1] - (a / b * mass[2]);
     return [mass[0], i , j];
